0a6be3b42bd616bece7ad93d1a7bc958bf771a97
OfficerYoda <officeryoda@gmail.com>
2022-10-15 17:47:16 +0200

    v 1.1.2 added a JDA CommandRegistrator constructor

--- src/main/java/de/officeryoda/dcbcr/Managment/CommandRegistrator.java
+++ src/main/java/de/officeryoda/dcbcr/Managment/CommandRegistrator.java
@@ -1,12 +1,17 @@
 package de.officeryoda.dcbcr.Managment;
 
 import de.officeryoda.dcbcr.Commands.PingCommands;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 public class CommandRegistrator {
 
     public CommandRegistrator(JDABuilder builder, String prefix) {
         builder.addEventListeners(new CommandListener(prefix));
+    }
+
+    public CommandRegistrator(JDA jda, String prefix) {
+        jda.addEventListener(new CommandListener(prefix));
     }
 
     public void registerPingCommand() {
